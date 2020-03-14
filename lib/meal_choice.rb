888d812a2a5d2f7998123d0e62d50a2435155434
Coral Fussman <coralfussman@gmail.com>
2020-03-14 17:48:35 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -6,6 +6,5 @@ def meal_choice(veg1, veg2, protein = 'tofu')
 
 end
 
-meal_choice('carrots', 'string beans', 'lentils')
-meal_choice('carrots', 'string beans')
-meal_choice("broccoli", "macaroni")
+meal_choice('broccoli', 'macaroni')
+meal_choice('broccoli', 'macaroni', 'roast beef')
